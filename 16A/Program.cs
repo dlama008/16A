560f6b17b7c987825b7e2bf14a2e72da3834bad2
friend <metalhead2112001@gmail.com>
2021-11-25 10:45:15 +0200

    Add project files.

---
+++ 16A/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _16A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method extension syntax
+            List<string> kaupungit = new List<string>() {  "Mikkeli",
+                "Savonlinna", "Jyväskylä", "Joensuu", "Kauhajoki", "Kouvola",
+                "Kannus", "Kajaani", "Kalajoki", "Kangasala", "Akaa"};
+            var poistetaan = kaupungit.Where(item => item.StartsWith("K"));
+            int lkm = kaupungit.Count;
+            if (poistetaan.Any())
+            {
+                for (int i = 0; i < lkm; i++)
+                {
+                    if (kaupungit[i].StartsWith("K"))
+                    {
+                        Console.Write("Poistetaanko {0} (K/E)?: ", kaupungit[i]);
+                        string valinta = Console.ReadLine().ToUpper();
+                        if (valinta.StartsWith("K"))
+                        {
+                            Console.WriteLine("Poistettiin {0}. ", kaupungit[i]);
+                            kaupungit.Remove(kaupungit[i]);
+                            lkm = lkm - 1;
+                            i = i - 1;
+                        }
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Ei löytynyt kaupunkeja.");
+
+            //Lopuksi:
+            Console.WriteLine("Kokoelmaan jäi: ");
+
+            foreach (var item in kaupungit)
+            {
+                Console.Write(item+" ");
+            }
+            
+        }
+    }
+}
